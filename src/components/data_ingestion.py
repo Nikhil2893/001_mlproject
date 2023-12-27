2d4cd36263e6ffc8ed137294eb728e01f7e135e6
Nikhil2893 <nikhilwakode2893@gmail.com>
2023-12-27 11:15:05 +0530

    Model training and hyperparameter training done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass   #this is used to create class variable in short
 
-from src.components.data_transformation import DataTransformation
+
 from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 #To define a class we need to use __init__, but in case of this decorator you will be able to directly define your class variable
@@ -54,5 +57,8 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
